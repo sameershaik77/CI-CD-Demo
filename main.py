f5b906ec49cda36c52360133230d12f778ba9294
sameershaik77 <s.md.sameer777@gmail.com>
2025-12-09 20:54:56 +0000

    CI/CD: add workflow, Dockerfile

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def root():
-    return {"status": "ok", "message":
-"Python FastAPI CI/CD works!"}
+def home():
+    return {"message": "CI/CD Deployment working!"}
